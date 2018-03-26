8a24a5eccc9e776ad27e5bc56ae23f894c38dfa6
ls1rius <865212941@qq.com>
2018-03-26 21:22:36 +0800

    加了跳帧，跳帧数为5

--- face_dataset.py
+++ face_dataset.py
@@ -1,21 +1,12 @@
-''''
-Capture multiple Faces from multiple users to be stored on a DataBase (dataset directory)
-	==> Faces will be stored on a directory: dataset/ (if does not exist, pls create one)
-	==> Each face will have a unique numeric integer ID as 1, 2, 3, etc
-
-Based on original code by Anirban Kar: https://github.com/thecodacus/Face-Recognition
-
-Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
-
-'''
-
 import cv2
 import os
+import time
 
 cam = cv2.VideoCapture(0)
 cam.set(3, 640)  # set video width
 cam.set(4, 480)  # set video height
 
+global index
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
@@ -25,34 +16,62 @@ face_id = 1
 
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
-count = 0
 
-while (True):
 
-    ret, img = cam.read()
+# Initialize the first fps
+t_start = time.time()
+fps = 0
+sfps = 0
+def get_faces( img ):
 
-    # img = cv2.flip(img, -1) # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    return faces, gray
 
+def draw_frame( img, faces, gray):
+    global index
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        count += 1
-
+        index += 1
         # Save the captured image into the datasets folder
         cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
-
     cv2.imshow('image', img)
 
-    k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
-    if k == 27:
-        break
-    elif count >= 30:  # Take 30 face sample and stop video
-        break
+if __name__ == '__main__':
+    global index
+    index = 0
+    count = 0
+    while (True):
+        #获取图像数据
+        ret, img = cam.read()
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
-cv2.destroyAllWindows()
+        #跳帧，跳了5帧
+        if count%5==0:
+            faces, gray= get_faces(img)
+        count = (count+1)%5
+
+        draw_frame(img, faces, gray)
+
+        k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif index >= 30:  # Take 30 face sample and stop video
+            break
+
+        cv2.putText(img, "FPS : " + str(int(sfps)), (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+
+
+        # Calculate and show the FPS
+        fps = fps + 1
+        sfps = fps / (time.time() - t_start)
+        print(sfps,fps)
+        print()
+
+
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
 
 
